03bb043d6865e370cad56b0d4ba117bc74d85aca
Anselme Habumugisha Irumva <andesanselme@gmail.com>
2023-03-05 19:57:38 +0200

    add: version support

---
+++ index.js
@@ -0,0 +1,6 @@
+var { submitHandler } = require("./src/submitHandler")
+
+module.exports = {
+  submitHandler,
+  version: "1.1.0"
+}
---
+++ src/submitHandler.js
@@ -0,0 +1,33 @@
+function clean(value) {
+  if (typeof value === 'string') {
+    if (value.length === 0) return null;
+
+    if (!isNaN(+value)) return parseInt(value)
+  }
+
+  return value;
+}
+
+function formToObject(form) {
+  let formData = new FormData(form);
+
+  let obj = {};
+
+  let entries = formData.entries();
+
+  for (let entry of entries) obj[entry[0]] = clean(entry[1]);
+
+  return obj;
+}
+
+function submitHandler(callback) {
+  return function (event) {
+    event.preventDefault();
+
+    callback(formToObject(event.target));
+  };
+}
+
+module.exports = {
+  submitHandler
+}
